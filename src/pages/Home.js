3f7ea997fa7ca11e023ab01eb475ad9eba97b8c9
HRush <hrkoren@gmail.com>
2021-06-02 21:07:20 -0400

    added pages folder and files

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../components/Home';
+
+function Home() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <button>View Employee List</button>
+        </div>
+    );
+}
+
+export default Home;
