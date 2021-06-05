9a96b56b9a1de7638aebb812fd211406b69d3f4b
HRush <hrkoren@gmail.com>
2021-06-05 07:36:58 -0400

    added and worked on Card component; added card info to employee card component

--- employee-directory/src/components/EmployeeCard/index.js
+++ employee-directory/src/components/EmployeeCard/index.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from './Card/index';
 
 
 function EmployeeCard() {
     const [employees, setEmployees] = useState();
 
     useEffect(() => {
-        axios('https://randomuser.me/api/?results=50').then((data) => {
+        axios('https://randomuser.me/api/?results=20&inc=name,location,email,dob,phone,id,picture').then((data) => {
             setEmployees(data);
-
-        });
+        })
+            .catch(err => {
+                console.log(err);
+            });
     }, []);
 
     console.log(employees)
 
     return (
-        <div></div>
+        <div className="flex flex-wrap">
+            {props.employees.map((employee, index) => {
+                return (<Card picture={employee.picture} name={employee.name} id={employee.id} email={employee.email} phone={employee.phone} location={employee.location} key={index} />)
+            })}
+        </div>
 
     )
 }
