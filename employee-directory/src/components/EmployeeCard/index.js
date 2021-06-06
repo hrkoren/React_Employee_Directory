71d0601c073543e84c0eca622863a461e26aa42c
HRush <hrkoren@gmail.com>
2021-06-06 14:42:15 -0400

    added bootstrap link, updated fonts, worked on styling

--- employee-directory/src/components/EmployeeCard/index.js
+++ employee-directory/src/components/EmployeeCard/index.js
@@ -4,7 +4,7 @@ import './style.css';
 function EmployeeCard(props) {
 console.log(props);
 return (
-    <table className='table table-sortable text-center'>
+    <table className='table striped table-sortable text-center'>
         <thead className='thead-dark'>
             <tr>
                 <th scope='col'>Image</th>
@@ -18,7 +18,7 @@ return (
         {props.employees.map((employee)=> {
             return (
             <tr>
-                <td><img src={employee.picture.thumbnail} alt='employee' /></td>
+                <td className='align-middle'><img src={employee.picture.thumbnail} alt='employee' /></td>
                 <td className='align-middle'>{employee.first} {employee.last}</td>
                 <td className='align-middle'>{employee.email}</td>
                 <td className='align-middle'>{employee.phone}</td>
