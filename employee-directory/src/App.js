975aad264ea797c08c19b09f8f205e98e4cd17ca
HRush <hrkoren@gmail.com>
2021-06-06 21:08:18 -0400

    fixed route in app.js and worked on styling

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Card from './components/Card';
-// import Home from './components/Home';
 
 function App() {
     return (
         <Router>
-            {/* <Route exact path='/' component={Home} /> */}
-            <Route exact path='/employee' component={Card} />
+            <Route exact path='/' component={Card} />
         </Router>
     )
 }
